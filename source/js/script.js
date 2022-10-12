1614dac5818f4881fcb6ef98f4052ef63bd9818d
AlexDrags <vera2052@mail.ru>
2022-10-12 16:48:24 +0300

    add footer and grid structure

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,7 @@
 const map = L.map('map').on('load', ()=>{console.log('Init map')}).setView({
-  lat:59.96840,
-  lng:30.3170209,
-}, 10);
+  lat:59.96819,
+  lng:30.31754,
+}, 19);
 
 L.tileLayer(
   'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
@@ -16,16 +16,20 @@ const mapPin = L.icon({
   iconAnchor: [19, 50],
 });
 
+const nameTitle = {
+  title: 'Санкт-Петербург, набережная реки Карповки, дом 5',
+}
+
 const marker = L.marker(
   {
     lat: 59.96831,
     lng: 30.31748,
   },
   {
-    draggable: true,
+    draggable: false,
     icon: mapPin,
   },
-).addTo(map);
+).addTo(map).bindPopup(nameTitle.title);
 
 marker.on('moveend', (evt) => {
   console.log(evt.target.getLatLng());
