3685e136a77d5e7049ec0e2be4ea16a0af3c643f
AlexDrags <vera2052@mail.ru>
2022-10-15 18:14:58 +0300

    many final fix

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,20 @@
+const sliderElement = document.querySelector('.uislider');
+const valueElement = document.querySelector('.price-form__value');
+
+valueElement.value = 0;
+
+noUiSlider.create(sliderElement, {
+  start: [0, 900],
+    connect: true,
+    range: {
+        'min': 0,
+        'max': 1100,
+    }
+});
+
+
+
+
 const swiper = new Swiper('.swiper', {
   // Optional parameters
   direction: 'horizontal',
@@ -57,5 +74,27 @@ marker.on('moveend', (evt) => {
   console.log(evt.target.getLatLng());
 });
 
+
+const mainNav = document.querySelector('.main-nav__list');
+const menuButton = document.querySelector('.button-js');
+const buttonOpen = document.querySelector('.menu-js');
+const buttonClose = document.querySelector('.close-js');
+
+
+if (mainNav.classList.contains('main-nav__list-nojs--open')) {
+  mainNav.classList.remove('main-nav__list-nojs--open');
+  buttonClose.classList.add('button-menu__icon--close');
+}
+
+
+
+function onClickHandler(evt) {
+  evt.preventDefault();
+  buttonOpen.classList.toggle('button-menu__icon--close');
+  buttonClose.classList.toggle('button-menu__icon--close');
+  mainNav.classList.toggle('main-nav__list--open-js');
+}
+
+menuButton.addEventListener('click', onClickHandler);
 
 
